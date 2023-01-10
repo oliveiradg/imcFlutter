96a7a1ad5add055ff51a4c047436735a4dca2182
João Oliveira <j.oliveiradg@gmail.com>
2023-01-10 10:12:02 -0300

    feat: criação pasta views, ciração themedata dark

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:imc/views/input_page_view.dart';
 
 void main() {
   runApp(CalculadoraIMC());
@@ -11,37 +12,20 @@ class CalculadoraIMC extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData.dark(),
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSwatch().copyWith(
+          primary: const Color(0xFF0A0E21),
+          secondary: Colors.purple,
+        ),
+        scaffoldBackgroundColor: const Color(0xFF0A0E21),
+        textTheme: const TextTheme(
+          bodyText2: TextStyle(
+           color:  Color(0xFFFFFFFF),
+          ),
+        ),
+      ),
       title: 'Calculadora IMC',
-      
-       
       home: const InputPage(),
     );
   }
 }
-
-class InputPage extends StatefulWidget {
-  const InputPage({Key? key}) : super(key: key);
-
-  @override
-  State<InputPage> createState() => _InputPageState();
-}
-
-class _InputPageState extends State<InputPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('BMI CALCULATOR'),
-      ),
-      body: Center(
-        child: Text('Body Text'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {  },
-        child: Icon(Icons.add),
-      ),
-    );
-  }
-} 
-  
